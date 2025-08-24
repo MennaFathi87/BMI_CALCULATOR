864e14ccbdcc64429d70da12cd80b928c087bfb3
MennaFathi <mnamohamed098@gmail.com>
2025-08-25 00:31:17 +0200

    add main

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:bmiapp/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BMI());
+}
+
+class BMI extends StatelessWidget {
+  const BMI({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
